d5208359e83cd077da4b1f17211aa9322a33ca15
hoangph1302 <hoangph1302@gmail.com>
2020-05-04 01:30:05 +0300

    completed 04/05

---
+++ lab2/code/lab2.py
@@ -0,0 +1,133 @@
+from PIL import Image
+import math
+
+def ConvertStringToBinary(string):
+    binary= ''
+    for i in string:
+        binary+=(format(ord(i), '08b'))
+    return binary
+
+def ConvertBinarryToString(binary):
+    string = ""
+    for i in range(0,len(binary),8):
+        integer = int(binary[i:i+8], 2)
+        character = chr(integer)
+        string += character
+    return string
+
+
+def ConvertDecimalToBinary(number):
+    binary=[]
+    binray= "{0:08b}".format(number)
+    return binray
+
+def ConvertBinaryToDecimal(binary):
+    dec=int(binary, 2)
+    return dec
+
+
+def ConvertDataToList(data,lenght):
+    dataList=[]
+    for i in range(0,lenght):
+        dataList+=list(data[i])
+    return dataList
+
+def ModifyData(data,message):
+    lenghtMessageBin=ConvertDecimalToBinary(len(message))
+    for i in range(0,8):
+        if(lenghtMessageBin[i]=='0'):
+          if(data[i]%2==1):
+            data[i]=data[i]-1
+        else:
+            if(data[i]%2==0):
+                data[i]=data[i]+1
+
+    messageBinary=ConvertStringToBinary(message)
+    for i in range(0,len(messageBinary)):
+        if(messageBinary[i]=='0'):
+            if(data[i+8]%2==1):
+                data[i+8]=data[i+8]-1
+        else:
+            if(data[i+8]%2==0):
+                data[i+8]=data[i+8]+1
+    return data
+
+
+def ReadData(data):
+    lengtMessageBinary=''
+    for i in range(0,8):
+        if(data[i]%2==0):
+            lengtMessageBinary+='0'
+        else:
+            lengtMessageBinary+='1'
+    lenghtMessage=ConvertBinaryToDecimal(lengtMessageBinary)
+    message=''
+    for i in range(8,8+lenghtMessage*8):
+        if(data[i]%2==0):
+            message+='0'
+        else:
+            message+='1'
+
+    return message
+
+
+def Encryto(fileImage,message):
+    image=Image.open(fileImage,'r')
+    newImage=image.copy()
+    width,height=newImage.size
+    data=ConvertDataToList(list(newImage.getdata()),height*width)
+
+    data=ModifyData(data,message)
+
+
+    dataImage=[]
+    for j in range(0,height*width*3,3):
+        dataImage.append(tuple(data[j:j+3]))
+    newImage.putdata(dataImage)
+    newImage.save('output.bmp')
+    image.close()
+    newImage.close()
+
+def Decrypto(fileImage):
+    image=Image.open(fileImage,'r')
+    width,height =image.size
+    data=ConvertDataToList(list(image.getdata()),height*width)
+    message=ConvertBinarryToString(ReadData(data))
+    file_secret = open('SecretText.txt',mode = 'w',encoding='UTF-8')
+    file_secret.write(message)
+    file_secret.close()
+    image.close()
+
+def PSNR(image1,image2):
+    imageAfer=Image.open(image1,'r')
+    imageBefor=Image.open(image2,'r')
+    width,height=imageAfer.size
+    dataAfter=ConvertDataToList(list(imageAfer.getdata()),height*width)
+    dataBefor=ConvertDataToList(list(imageBefor.getdata()),height*width)
+    sum=0
+    for i in range(0,len(dataAfter)):
+        sum+=math.pow((dataAfter[i]-dataBefor[i]),2)
+    MSE=sum/(width*height)
+    PSNR=10*math.log((255*255/MSE),10)
+    return PSNR
+
+image1='input.bmp'
+image2='output.bmp'
+
+ ##---------------------------------------main programme-----------------------------------------------------------------
+print('-------------------------Steganography LSB--------------------------------------')
+print ('1.steganography'+'\n'+'2.Find scret text from image \n'+'3.PSNR\n')
+option=int(input())
+if(option==1):
+     #image=input('Enter file image!\n')
+     message=input('Enter secret text!\n')
+     Encryto(image1,message)
+elif( option==2):
+     Decrypto(image2)
+     print ('Please Open file SecretText!')
+elif(option==3):
+    print ('PSNR:')
+    print (PSNR('input.bmp','output.bmp'))
+
+
+
